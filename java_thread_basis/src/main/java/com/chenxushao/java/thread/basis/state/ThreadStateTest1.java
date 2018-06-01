b24c88b991fa4e015a88204d820848dfe6449d35
chenxushao <418967511@qq.com>
2018-06-01 10:05:27 +0800

    add:线程状态演示

---
+++ java_thread_basis/src/main/java/com/chenxushao/java/thread/basis/state/ThreadStateTest1.java
@@ -0,0 +1,45 @@
+package com.chenxushao.java.thread.basis.state;
+
+/**
+ * @author chenxushao
+ */
+public class ThreadStateTest1 {
+
+    public static void main(String[] args) throws Exception{
+
+        Thread t1 = new Thread(new Task1(),"t1");
+
+        System.out.println("线程"+t1.getName()+"状态:"+ t1.getState());
+        t1.start();
+
+        System.out.println("线程"+t1.getName()+"状态:"+ t1.getState());
+
+        Thread.sleep(3 * 1000);
+        System.out.println("线程" + t1.getName() + "状态:" + t1.getState());
+
+
+        Thread.sleep(20 * 1000);
+        System.out.println("线程" + t1.getName() + "状态:" + t1.getState());
+
+        System.in.read();
+
+
+    }
+
+
+    private static class Task1 implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000 * 20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Thread currentThread = Thread.currentThread();
+            System.out.println(currentThread.getName() + " running...");
+
+        }
+    }
+
+}
