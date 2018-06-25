d8783a0f6306774db98caa0b174f4a87a8c8d767
chenxushao <418967511@qq.com>
2018-06-25 21:25:42 +0800

    refactor：完善juc范例

--- java_juc/src/main/java/com/chenxushao/java/juc/callable/CallableDemo1.java
+++ java_juc/src/main/java/com/chenxushao/java/juc/callable/CallableDemo1.java
@@ -38,23 +38,24 @@ public class CallableDemo1 {
 			}
 		}
 	}
-}
 
-class TaskWithResult implements Callable<String> {
-	private int id;
 
-	public TaskWithResult(int id) {
-		this.id = id;
-	}
+	private static class TaskWithResult implements Callable<String> {
+		private int id;
 
-	/**
-	 * 任务的具体过程，一旦任务传给ExecutorService的submit方法， 则该方法自动在一个线程上执行
-	 */
-	public String call() throws Exception {
-		System.out.println("call()方法被自动调用！！！    "
+		public TaskWithResult(int id) {
+			this.id = id;
+		}
+
+		/**
+		 * 任务的具体过程，一旦任务传给ExecutorService的submit方法， 则该方法自动在一个线程上执行
+		 */
+		public String call() throws Exception {
+			System.out.println("call()方法被自动调用！！！    "
 				+ Thread.currentThread().getName());
-		// 该返回结果将被Future的get方法得到
-		return "call()方法被自动调用，任务返回的结果是：" + id + "    "
+			// 该返回结果将被Future的get方法得到
+			return "call()方法被自动调用，任务返回的结果是：" + id + "    "
 				+ Thread.currentThread().getName();
+		}
 	}
 }
